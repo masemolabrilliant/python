edb920469ce0295765b65ee24677c389d2409176
Mogaladi Brilliant <masemola.m98@gmail.com>
2025-02-17 10:22:56 +0200

    variables

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hellow sir me")
+# variables string , int floast boolean
+#string
+firms_name = "Kwetepe"
+fruit = "Apple"
+print(f"Hello {firms_name}")
+
+print(f" {firms_name} loves a fruit called {fruit}")
+#intergers
+age =20
+print(f"you are {age} years old")
