db480e11adadc9986250aff969950bd5054c0b82
Mogaladi Brilliant <masemola.m98@gmail.com>
2025-02-17 12:31:01 +0200

    type cast user input

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#inputes jsing ipunte()
+# user input using input()
+
+name =input("Hello, what is your name? : ")
+age = int(input("how old are you? : "))
+
+print(f"Hi, my name is {name} and i amd {age} years old")
+
+age +=1
+
+print(f"did you knw by next year you will be turning {age} years old")
